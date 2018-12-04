6ec9b850ceb97e4ba5434505d3c1234d95151f85
DBlackFox <arvin.kosasih@my.jcu.edu.au>
2018-12-04 11:02:00 +0800

    Practical5
    Do feedback

---
+++ color_codes.py
@@ -0,0 +1,7 @@
+color_codes = {"bisque1": "#ffe4c4", "red": "#ee3b3b", "blue": "#98f5ff", "green": "#76ee00", "forestgreen" : "#228b22"}
+
+color_name = input("Enter the name of the color ").lower()
+while color_name != "":
+    print("The code for \"{}\" is {}".format(color_name, color_codes.get(color_name)))
+    color_name = input("Enter the name of the color: ")
+    
