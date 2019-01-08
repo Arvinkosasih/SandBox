ba6c3e233f018c7ed7c7f0ffb6e21c8eb2ce1f45
DBlackFox <arvin.kosasih@my.jcu.edu.au>
2019-01-08 09:38:59 +0800

    Pracs
    forgot commit

--- Practical 6 task 1.py
+++ Practical 6 task 1.py
@@ -1,13 +1,22 @@
-def main():
-    x = int(input("Enter x value "))
-    y = int(input("Enter y value "))
-    doMath(x,y)
+from car import Car
 
-def doMath(lhs, rhs):
-    sum = lhs + rhs
-    diff = lhs - rhs
-    product = lhs * rhs
-    quotient = lhs / rhs
-    print("Sum: "+ str(sum) + " diff: "+ str(diff)+ " product: " + str(product)+ " quotient: " +str(quotient))
+
+def main():
+    """Demo test code to show how to use car class."""
+    my_car = Car("My car", 180)
+    my_car.drive(30)
+    print("fuel =", my_car.fuel)
+    print("odo =", my_car.odometer)
+    print(my_car)
+
+    print("Car {}, {}".format(my_car.fuel, my_car.odometer))
+    print("Car {self.fuel}, {self.odometer}".format(self=my_car))
+
+    limo = Car("Limo", 100)
+    limo.add_fuel(20)
+    print(limo.fuel)
+    limo.drive(115)
+    print(limo.odometer)
+
 
 main()
